1dafa1957010d413a255e485b2239c97c6c0bdbd
Alex Rugama <alrugama@ucsc.edu>
2021-06-25 13:10:17 -0700

    first save

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,12 +2,21 @@ class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
     }
-}
 
-init(){
+    init() {
 
-}
-preload(){
+    }
 
+    preload() {
+
+    }
+    
+    create(){
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
+    }
+
+    update(){
+    
+    }
 }
-Create()
