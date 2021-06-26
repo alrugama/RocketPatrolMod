067aaf6921603c3de4fb52ee3d059d277b6cd275
Alex Rugama <alrugama@ucsc.edu>
2021-06-25 22:03:07 -0700

    Added ability to restart

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -68,16 +68,34 @@ class Play extends Phaser.Scene {
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score,
             scoreConfig);
+
+        //GAME OVER flag
+        this.gameOver = false;
+
+        //60-sec play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
+            scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
     
     update(){
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
-
+        
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
+        
         //update enemy
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if(!this.gameOver){
+            this.p1Rocket.update();
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
 
         //Check collision
         if(this.checkCollision(this.p1Rocket, this.ship03)){
