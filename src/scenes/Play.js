bf0120416bc83f742a3432d55dc332866d1b0a33
Alex Rugama <alrugama@ucsc.edu>
2021-06-25 19:34:14 -0700

    added rocket and movement

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,10 +25,19 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+
+        //add rocket(p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'roccket').setOrigin(0.5, 0);
+
+        //Define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.keyboard.KeyCodes.RIGHT);
     }
     
     update(){
-        this.starfield.tilePositioX -= 4;
-    
+        this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
     }
 }
