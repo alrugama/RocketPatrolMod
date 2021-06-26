2e7a98f44a760c179216cc2dea8939462a32c129
Alex Rugama <alrugama@ucsc.edu>
2021-06-25 22:51:23 -0700

    Completed game only need to be added to web.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -74,9 +74,9 @@ class Play extends Phaser.Scene {
 
         //60-sec play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu',
             scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
@@ -85,10 +85,10 @@ class Play extends Phaser.Scene {
     update(){
         this.starfield.tilePositionX -= 4;
         
-        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
-            this.scene.restart();
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start('menuScene');
         }
-        
+
         //update enemy
         if(!this.gameOver){
             this.p1Rocket.update();
@@ -143,5 +143,6 @@ class Play extends Phaser.Scene {
         //Score addition and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        this.sound.play('sfx_explosion');
     }
 }
