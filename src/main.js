ae9251f2a8531e8f38549a7f5336065849606c03
Alex Rugama <alrugama@ucsc.edu>
2021-07-01 23:46:25 -0700

    Added comments and name to main.js

--- src/main.js
+++ src/main.js
@@ -13,3 +13,21 @@ let borderPadding = borderUISize / 3;
 
 //Reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+////////////////////////////////////////
+// Name: Alex Rugama
+// Project: Rocket Partol Mod 
+// Date: 07/01/2021
+// Time: About 15 hours to complete
+//
+//---------------- Points breakdown------------------------------------------------------
+// 1)trackes High score: 5 points
+// 2)Created my own backgound music: 5 points
+// 3)Implemented speed increase after 30 second: 5 points
+// 4)New scrolling background 5 points
+// 5)Allows player to control rocket: 5 points
+// 6)Four new sfx that are randomized: 10 points
+// 7)Displaying time at the top of the screen: 10 points
+// 8)Created new faster and smaller enemy: 20 points
+// 9)All new artwork and animations for game assets: 20 points
+// total points 85 + 20 for tutorial completion 
